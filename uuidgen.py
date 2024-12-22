dc1f3a79138e3c4c20902ce04a59f39d82d0b568
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-22 07:53:37 -0500

    Update uuidgen.py

--- uuidgen.py
+++ uuidgen.py
@@ -7,6 +7,7 @@
 # ///
 
 import uuid
+import typing as t
 
 import hachitool
 from pydantic import Field, field_validator, model_validator
@@ -14,20 +15,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class UUIDGenerator(BaseSettings):
+    namespace: uuid.UUID | t.Literal["dns", "oid", "url", "x500"] = Field(default_factory=uuid.uuid4)
     name: str = Field(default_factory=lambda: uuid.uuid4().hex)
-    namespace: uuid.UUID = Field(default_factory=uuid.uuid4)
 
     def generate(self):
         return uuid.uuid5(self.namespace, self.name)
 
-    @field_validator("namespace", mode="before")
+    @field_validator("namespace")
     def validate_namespace(cls, v):
-        if str(v).startswith("spec::"):
-            try:
-                return getattr(uuid, "NAMESPACE_" + v.split("::")[1].upper())
-            except AttributeError:
-                pass
-        
-        return v
+        return getattr(uuid, "NAMESPACE_{v.upper()}", v)
 
 hachitool.set_output(uuid=UUIDGenerator().generate())
