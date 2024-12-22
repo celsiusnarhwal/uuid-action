19527ee856cd7a9cf90a9a12fe3b045674f4da4d
celsius narhwal <hello@celsiusnarhwal.dev>
2024-12-22 12:10:12 -0500

    Update uuidgen.py

--- uuidgen.py
+++ uuidgen.py
@@ -6,23 +6,39 @@
 # ]
 # ///
 
-import uuid
 import typing as t
+import uuid
 
 import hachitool
-from pydantic import Field, field_validator, model_validator
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field, TypeAdapter, field_validator
+from pydantic_settings import BaseSettings
 
 
 class UUIDGenerator(BaseSettings):
-    namespace: uuid.UUID | t.Literal["dns", "oid", "url", "x500"] = Field(default_factory=uuid.uuid4)
+    namespace: uuid.UUID | t.Literal["dns", "oid", "url", "x500"] = Field(
+        default_factory=uuid.uuid4
+    )
     name: str = Field(default_factory=lambda: uuid.uuid4().hex)
 
-    def generate(self):
+    def generate(self) -> uuid.UUID:
         return uuid.uuid5(self.namespace, self.name)
 
     @field_validator("namespace")
     def validate_namespace(cls, v):
         return getattr(uuid, "NAMESPACE_{v.upper()}", v)
 
-hachitool.set_output(uuid=UUIDGenerator().generate())
+
+generator = UUIDGenerator()
+identifier = generator.generate()
+
+output = {
+    "uuid": identifier,
+    "hex": identifier.hex,
+    "int": identifier.int,
+    "urn": identifier.urn,
+    "namespace": generator.namespace,
+    "name": generator.name,
+}
+
+
+hachitool.set_output(**output, json=TypeAdapter(dict).dump_json(output).decode())
